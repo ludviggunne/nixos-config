e14c7af56a247c1a189c9487ca236c49b298adef
ludviggl <ludvig.gunne.lindstrom.dev@proton.me>
2024-06-22 12:44:15 +0200

    ranger

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -9,6 +9,8 @@
     zsh
     oh-my-zsh
     eza
+    ripgrep
+    ranger
   ];
 
   programs.zsh = {
@@ -20,15 +22,17 @@
       update = "sudo nixos-rebuild switch --flake ~/nixos-config/#${host}";
       config = "hx ~/nixos-config";
       ls = "eza -l";
+      rn = "ranger";
     };
 
     initExtra = ''
       eval "$(direnv hook zsh)"
+      export EDITOR=hx
     '';
 
     oh-my-zsh = {
       enable = true;
-      plugins = [ "git" ];
+      plugins = [ "git" "fzf" ];
       theme = "afowler";
     };
   };
